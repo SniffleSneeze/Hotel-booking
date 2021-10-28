4959d0429923675212a018a63f82235362f10818
SniffleSneeze <70772425+SniffleSneeze@users.noreply.github.com>
2021-10-28 17:03:20 +0100

    bookingController method done + view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ShowRoomsController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,8 +31,25 @@ Route::get('/test', function() {return "Goodbye";});
 
 Route::get('/rooms', ShowRoomsController::class);
 
-Route::get('/bookings',[App\Http\Controllers\BookingController::class, 'index']);
-Route::get('/bookings/create',[App\Http\Controllers\BookingController::class, 'create']);
-Route::post('/bookings',[App\Http\Controllers\BookingController::class, 'store']);
+
+/**
+ * this is the traditional way to do a route in an MVC framework,
+ * you would set a rout for each different action.
+ */
+//Route::get('/bookings',[App\Http\Controllers\BookingController::class, 'index']);
+//Route::get('/bookings/create',[App\Http\Controllers\BookingController::class, 'create']);
+//Route::get('/bookings/{booking}',[App\Http\Controllers\BookingController::class, 'show']);
+//Route::get('/bookings/{booking}/edit',[App\Http\Controllers\BookingController::class, 'edit']);
+//Route::post('/bookings',[App\Http\Controllers\BookingController::class, 'store']);
+//Route::put('/bookings/{booking}',[App\Http\Controllers\BookingController::class, 'update']);
+//Route::delete('/bookings/{booking}',[App\Http\Controllers\BookingController::class, 'destroy']);
+
+
+/**
+ * Laravel allow us to compact all those rout in one line of code.
+ */
+Route::resource('bookings',App\Http\Controllers\BookingController::class);
+
+
 
 
